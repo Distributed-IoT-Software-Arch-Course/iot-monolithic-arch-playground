0c2953bc8d6ddd47d6139e5d0457a533e118e3a1
Marco Picone <picone.m@gmail.com>
2024-11-17 10:57:00 +0100

    Web Server Added

--- application/web/web_server.py
+++ application/web/web_server.py
@@ -1,29 +1,94 @@
-from flask import Flask, render_template
-from data.data_manager import DataManager
+from flask import Flask, request, render_template
 import os
+import yaml
+import threading
 
-# Get the main application directory
-main_app_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+class WebServer:
 
-# Construct the file path
-template_dir = os.path.join(main_app_path, 'presentation')
+    def __init__(self, config_file, data_manager):
 
-# Create the Flask app
-app = Flask(__name__, template_folder=template_dir)
+        # Server Thread
+        self.server_thread = None
 
-# Initialize DataManager
-data_manager = DataManager()
-data_manager.init_demo_data()
+        # Save the data manager
+        self.data_manager = data_manager
 
-@app.route('/locations')
-def locations():
-    location_list = data_manager.get_all_locations()
-    return render_template('locations.html', locations=location_list)
+        # Save the configuration file
+        self.config_file = config_file
 
-@app.route('/location/<string:location_id>/devices')
-def devices(location_id):
-    device_list = data_manager.get_devices_by_location(location_id)
-    return render_template('devices.html', devices=device_list, location_id=location_id)
+        # Get the main application directory
+        main_app_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=7071)
+        # Construct the file path
+        template_dir = os.path.join(main_app_path, 'presentation')
+
+        # Set a default configuration
+        self.configuration_dict = {
+            "web": {
+                "host": "0.0.0.0",
+                "port": 7071
+            }
+        }
+
+        # Read Configuration from target Configuration File Path
+        self.read_configuration_file()
+
+        # Create the Flask app
+        self.app = Flask(__name__, template_folder=template_dir)
+
+        # Add URL rules to the Flask app mapping the URL to the function
+        self.app.add_url_rule('/locations', 'locations', self.locations)
+        self.app.add_url_rule('/location/<string:location_id>/devices', 'devices', self.devices)
+
+    def read_configuration_file(self):
+        """ Read Configuration File for the Web Server
+         :return:
+        """
+
+        # Get the main application directory
+        main_app_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+        # Construct the file path
+        file_path = os.path.join(main_app_path, self.config_file)
+
+        with open(file_path, 'r') as file:
+            self.configuration_dict = yaml.safe_load(file)
+
+        print("Read Configuration from file ({}): {}".format(self.config_file, self.configuration_dict))
+
+    def locations(self):
+        """ Get all locations and render the locations.html template"""
+        location_list = self.data_manager.get_all_locations()
+        return render_template('locations.html', locations=location_list)
+
+    def devices(self, location_id):
+        """ Get all devices for a specific location and render the devices.html template"""
+        device_list = self.data_manager.get_devices_by_location(location_id)
+        return render_template('devices.html', devices=device_list, location_id=location_id)
+
+    def run_server(self):
+        """ Run the Flask Web Server"""
+        self.app.run(host=self.configuration_dict['web']['host'], port=self.configuration_dict['web']['port'])
+
+    def start(self):
+        self.server_thread = threading.Thread(target=self.run_server)
+        self.server_thread.start()
+
+    def stop(self):
+        """ Stop the REST API Server (Flask Method)
+        In this code, request.environ.get('werkzeug.server.shutdown')
+        retrieves the shutdown function from the environment.
+        If the function is not found, it raises a RuntimeError,
+        indicating that the server is not running with Werkzeug.
+        If the function is found, it is called to shut down the server."""
+
+        # Shutdown the server
+        func = request.environ.get('werkzeug.server.shutdown')
+        if func is None:
+            raise RuntimeError('Not running with the Werkzeug Server')
+
+        # Call the shutdown function
+        func()
+
+        # Wait for the server thread to join
+        self.server_thread.join()
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from data.data_manager import DataManager
-from logic.model.location_model import LocationModel
-from logic.model.device_model import DeviceModel
 from application.api.rest_api_server import RestApiServer
+from application.web.web_server import WebServer
 
 API_CONFIG_FILE = "config/api_conf.yaml"
+WEB_CONFIG_FILE = "config/web_conf.yaml"
 
 if __name__ == '__main__':
 
@@ -11,10 +11,16 @@ if __name__ == '__main__':
     data_manager = DataManager()
 
     # Init some demo data on the Data Manager
-
+    data_manager.init_demo_data()
 
     # Create RESTful API Server
     rest_api_server = RestApiServer(API_CONFIG_FILE, data_manager)
 
     # Run RESTful API Server
     rest_api_server.start()
+
+    # Create Web Server
+    web_server = WebServer(WEB_CONFIG_FILE, data_manager)
+
+    # Run Web Server
+    web_server.start()
